8eeb74079de4a30b9db4f340421edc6765df2df3
Sorcerer14 <rajaditya9151@gmail.com>
2024-02-02 03:34:37 +0530

    Created the live match components tab
    Signed-off-by: Sorcerer14 <rajaditya9151@gmail.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LeaguePointTableComponent } from './pages/league-point-table/league-point-table.component';
+import { LiveScoreComponent } from './pages/live-score/live-score.component';
+import { MatchHistoryComponent } from './pages/match-history/match-history.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "home",
+        component: HomeComponent,
+        title: "Home | CrickGuru"
+    },
+    {
+        path: "match-history",
+        component: MatchHistoryComponent,
+        title: "Match-History | CrickGuru"
+    },
+    {
+        path: "live-scores",
+        component: LiveScoreComponent,
+        title: "Live-Scores | CrickGuru"
+    },
+    {
+        path: "point-table",
+        component: LeaguePointTableComponent,
+        title: "Points-Table | CrickGuru"
+    },
+];
---
+++ src/app/pages/match-history/match-history.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-match-history',
+  standalone: true,
+  imports: [],
+  templateUrl: './match-history.component.html',
+  styleUrl: './match-history.component.css'
+})
+export class MatchHistoryComponent {
+
+}
